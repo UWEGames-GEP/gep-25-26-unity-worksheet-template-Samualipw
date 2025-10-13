7dd3333edcb0ae28130bb28cdffe38f5dc2fb269
Samualipw <Samual2.Wilson@live.uwe.ac.uk>
2025-10-13 16:23:47 +0100

    Base Commit + Pause Button

---
+++ Assets/Scripts/MB_GameManager.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+
+
+
+public enum GameState
+{
+    GAMEPLAY,
+    PAUSE
+}
+
+
+
+
+public class MB_GameManager : MonoBehaviour
+{
+
+
+    public GameState state = GameState.GAMEPLAY;
+
+    bool hasChangedState = false;
+    
+ 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == GameState.GAMEPLAY) 
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                state = GameState.PAUSE;
+                hasChangedState = true;
+            }
+        }
+
+        else if (state == GameState.PAUSE)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape)) 
+            {
+                state = GameState.GAMEPLAY;
+                hasChangedState = true;
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (hasChangedState) 
+        {
+            hasChangedState = false;
+
+            switch (state) 
+            {
+                case GameState.GAMEPLAY:
+                    Time.timeScale = 1.0f;
+                    break;
+                
+                case GameState.PAUSE:
+                    Time.timeScale = 0.0f;
+                    break;
+            }
+        }
+    }
+}
