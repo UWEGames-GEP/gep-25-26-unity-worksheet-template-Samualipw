a6634619cc73f3bab12c7a89a3a78f14ca952db8
Samualipw <Samual2.Wilson@live.uwe.ac.uk>
2025-12-07 22:44:15 +0000

    Added Unity input mapping

---
+++ Assets/Scripts/MB_PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using StarterAssets;
+using UnityEngine.InputSystem;
+
+public class MB_PlayerController : ThirdPersonController
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public MB_GameManager gameManager;
+    private void OnPause(InputValue value)
+    {
+        if (value.isPressed) 
+        {
+            Debug.Log("pause game");
+        }
+    }
+
+
+
+    
+}
